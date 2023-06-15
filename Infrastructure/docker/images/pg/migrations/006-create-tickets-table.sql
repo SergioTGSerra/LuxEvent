aa0355edf70181ec6fa8e75803351ae3028fc7c1
Sérgio Serra <sergioserra.2003@gmail.com>
2023-06-15 20:32:52 +0100

    Update Bug Migrations

--- Infrastructure/docker/images/pg/migrations/006-create-tickets-table.sql
+++ Infrastructure/docker/images/pg/migrations/006-create-tickets-table.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE tickets (
     event_id         uuid REFERENCES events(id),
-    ticker_type_id   uuid REFERENCES ticket_types(id),
+    ticket_type_id   uuid REFERENCES ticket_types(id),
     price            DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (event_id, ticker_type_id)
 );
