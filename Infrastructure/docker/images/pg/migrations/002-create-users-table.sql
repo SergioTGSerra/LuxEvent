895e21ed6294d1432059ba1b1dfe5ee5134e199f
Sérgio Serra <sergioserra.2003@gmail.com>
2023-06-15 20:23:31 +0100

    Update Migrations

--- Infrastructure/docker/images/pg/migrations/002-create-users-table.sql
+++ Infrastructure/docker/images/pg/migrations/002-create-users-table.sql
@@ -3,6 +3,5 @@ CREATE TABLE users (
      name           VARCHAR(100) NOT NULL,
      username       VARCHAR(100) NOT NULL,
      password       VARCHAR(100) NOT NULL,
-     email          VARCHAR(100) NOT NULL,
-     birth_date     DATE NOT NULL
+     email          VARCHAR(100) NOT NULL
 );
