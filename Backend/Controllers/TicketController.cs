f363d8c2c3d4ca0a67f798eb677051057e2db6ec
Sérgio Serra <sergioserra.2003@gmail.com>
2023-06-16 18:14:10 +0100

    Ticket BLL adn Back

---
+++ Backend/Controllers/TicketController.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TicketsController : ControllerBase
+{
+    private readonly TicketService _ticketService;
+
+    public TicketsController(TicketService ticketService)
+    {
+        _ticketService = ticketService;
+    }
+
+    [HttpPost]
+    public IActionResult CreateTicket(Guid eventId, Guid ticketTypeId, decimal price)
+    {
+        var ticket = _ticketService.CreateTicket(eventId, ticketTypeId, price);
+        return Ok(ticket);
+    }
+
+    [HttpGet("{ticketId}")]
+    public IActionResult GetTicket(Guid ticketId)
+    {
+        var ticket = _ticketService.GetTicket(ticketId);
+        if (ticket == null)
+            return NotFound();
+
+        return Ok(ticket);
+    }
+
+    [HttpGet]
+    public IActionResult GetAllTickets()
+    {
+        var tickets = _ticketService.GetAllTickets();
+        return Ok(tickets);
+    }
+
+    [HttpPut("{ticketId}")]
+    public IActionResult UpdateTicket(Guid ticketId, decimal price)
+    {
+        _ticketService.UpdateTicket(ticketId, price);
+        return NoContent();
+    }
+
+    [HttpDelete("{ticketId}")]
+    public IActionResult DeleteTicket(Guid ticketId)
+    {
+        _ticketService.DeleteTicket(ticketId);
+        return NoContent();
+    }
+}
---
+++ BusinessLogic/Services/TicketService.cs
@@ -0,0 +1,60 @@
+﻿using BusinessLogic.Context;
+using BusinessLogic.Entities;
+
+namespace BusinessLogic.Services;
+
+public class TicketService
+{
+    private readonly ES2DbContext _dbContext;
+
+    public TicketService(ES2DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Ticket CreateTicket(Guid eventId, Guid ticketTypeId, decimal price)
+    {
+        var ticket = new Ticket
+        {
+            Id = Guid.NewGuid(),
+            EventId = eventId,
+            TicketTypeId = ticketTypeId,
+            Price = price
+        };
+
+        _dbContext.Tickets.Add(ticket);
+        _dbContext.SaveChanges();
+
+        return ticket;
+    }
+
+    public Ticket GetTicket(Guid ticketId)
+    {
+        return _dbContext.Tickets.FirstOrDefault(t => t.Id == ticketId);
+    }
+
+    public IEnumerable<Ticket> GetAllTickets()
+    {
+        return _dbContext.Tickets.ToList();
+    }
+
+    public void UpdateTicket(Guid ticketId, decimal price)
+    {
+        var ticket = _dbContext.Tickets.FirstOrDefault(t => t.Id == ticketId);
+        if (ticket != null)
+        {
+            ticket.Price = price;
+            _dbContext.SaveChanges();
+        }
+    }
+
+    public void DeleteTicket(Guid ticketId)
+    {
+        var ticket = _dbContext.Tickets.FirstOrDefault(t => t.Id == ticketId);
+        if (ticket != null)
+        {
+            _dbContext.Tickets.Remove(ticket);
+            _dbContext.SaveChanges();
+        }
+    }
+}
