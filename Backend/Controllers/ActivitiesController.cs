4bc4f5edfff3ab7677ddf13f368961aebdc093c2
serra <sergioserra.2003@gmail.com>
2023-06-16 12:41:42 +0100

    Activity BackEnd

---
+++ Backend/Controllers/ActivitiesController.cs
@@ -0,0 +1,63 @@
+﻿using BusinessLogic.Entities;
+using BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivityController : ControllerBase
+    {
+        private readonly ActivityService activityService;
+
+        public ActivityController(ActivityService activityService)
+        {
+            this.activityService = activityService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            var activities = await activityService.GetActivitiesAsync();
+            return Ok(activities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivityById(Guid id)
+        {
+            var activity = await activityService.GetActivityByIdAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            return Ok(activity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await activityService.CreateActivityAsync(activity);
+            return CreatedAtAction(nameof(GetActivityById), new { id = activity.Id }, activity);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateActivity(Guid id, Activity activity)
+        {
+            if (id != activity.Id)
+            {
+                return BadRequest();
+            }
+
+            await activityService.UpdateActivityAsync(activity);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            await activityService.DeleteActivityAsync(id);
+            return NoContent();
+        }
+    }
+
+}
---
+++ BusinessLogic/Services/ActivityService.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLogic.Context;
+using BusinessLogic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Services;
+
+public class ActivityService
+{
+    private readonly ES2DbContext dbContext;
+
+    public ActivityService(ES2DbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<List<Activity>> GetActivitiesAsync()
+    {
+        return await dbContext.Activities.ToListAsync();
+    }
+
+    public async Task<Activity> GetActivityByIdAsync(Guid id)
+    {
+        return await dbContext.Activities.FindAsync(id);
+    }
+
+    public async Task CreateActivityAsync(Activity activity)
+    {
+        dbContext.Activities.Add(activity);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task UpdateActivityAsync(Activity activity)
+    {
+        dbContext.Entry(activity).State = EntityState.Modified;
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteActivityAsync(Guid id)
+    {
+        var activity = await dbContext.Activities.FindAsync(id);
+        if (activity != null)
+        {
+            dbContext.Activities.Remove(activity);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
