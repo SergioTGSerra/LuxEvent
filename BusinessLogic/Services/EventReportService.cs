765f04a8626c320bab173ef4b28814e114e09d5f
Sérgio Serra <sergioserra.2003@gmail.com>
2023-06-18 11:00:00 +0100

    Update name front-end

---
+++ BusinessLogic/Services/EventReportService.cs
@@ -0,0 +1,32 @@
+﻿using BusinessLogic.Context;
+using BusinessLogic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic.Services;
+
+public class EventReportService
+{
+    private readonly ES2DbContext _dbContext;
+
+    public EventReportService(ES2DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<int> GetEventCountByCategory(Guid categoryId)
+    {
+        return await _dbContext.Events.CountAsync(e => e.CategoryId == categoryId);
+    }
+
+    public async Task<IEnumerable<Event>> GetMostPopularEvents(int count)
+    {
+        return await _dbContext.Events.OrderByDescending(e => e.RegistrationsEvents.Count)
+            .Take(count)
+            .ToListAsync();
+    }
+
+    public async Task<int> GetTotalParticipantsCount()
+    {
+        return await _dbContext.RegistrationsEvents.CountAsync();
+    }
+}
