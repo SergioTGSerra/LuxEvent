3d7ddc97b2c22e941d3124819c00176ebf7f8195
Sérgio Serra <sergioserra.2003@gmail.com>
2023-06-21 12:39:51 +0100

    Update Migrations

--- Infrastructure/docker/images/pg/migrations/006-create-tickets-table.sql
+++ Infrastructure/docker/images/pg/migrations/006-create-tickets-table.sql
@@ -2,5 +2,6 @@
     id               uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     event_id         uuid REFERENCES events(id),
     ticket_type_id   uuid REFERENCES ticket_types(id),
-    price            DECIMAL(10, 2) NOT NULL
+    price            DECIMAL(10, 2) NOT NULL,
+    max_participants INTEGER
 );
--- Infrastructure/docker/images/pg/migrations/009-create-registrations-events-table.sql
+++ Infrastructure/docker/images/pg/migrations/009-create-registrations-events-table.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE registrations_events (
     id              uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     user_id         uuid REFERENCES users(id),
-    event_id        uuid REFERENCES events(id)
+    event_id        uuid REFERENCES events(id),
+    ticket_id       uuid REFERENCES tickets(id)
 );
